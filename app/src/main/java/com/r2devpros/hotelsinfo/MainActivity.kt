57487d5f28d86643faa77c5b47ab59d3b434cbe0
Saul-mdoR2 <75604861+Saul-mdoR2@users.noreply.github.com>
2022-06-28 16:00:23 -0500

    drawing map

--- app/src/main/java/com/r2devpros/hotelsinfo/MainActivity.kt
+++ app/src/main/java/com/r2devpros/hotelsinfo/MainActivity.kt
@@ -3,6 +3,9 @@ package com.r2devpros.hotelsinfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.r2devpros.hotelsinfo.repository.remote.handle
 import com.r2devpros.hotelsinfo.repository.remote.hotelServer.HotelServiceManager
 import kotlinx.coroutines.CoroutineScope
@@ -11,8 +14,10 @@ import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
 import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
     private val hotelServiceManager: HotelServiceManager by inject()
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,9 +47,20 @@ class MainActivity : AppCompatActivity() {
                     Timber.d("MainActivity_TAG: callWebService: error: $it")
                 }, success = {
                     Timber.d("MainActivity_TAG: callWebService: success: ${it.size}")
+                    createMap()
                 }
             )
         }
     }
 
+    private fun createMap() {
+        Timber.d("MainActivity_TAG: createMap: ")
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+    }
 }
